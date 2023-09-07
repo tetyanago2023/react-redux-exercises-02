1268f3ead7aac19cadc226aad67cfa49e9b122cb
go2023 <tetyanago2018@gmail.com>
2023-09-06 22:33:26 -0700

    ReactDom.render => ReactDom.createRoot

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { store } from './app/store';
@@ -8,11 +8,11 @@ import { fetchUsers } from './features/users/usersSlice';
 
 store.dispatch(fetchUsers());
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <React.StrictMode>
         <Provider store={store}>
             <App />
         </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
